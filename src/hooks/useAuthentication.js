d1bafa416359d52394c5fed2cdbecb1df938949a
gabrielsmm <gabrielsmm251@gmail.com>
2023-01-29 13:57:12 -0300

    Login de usuário

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -62,11 +62,34 @@ export const useAuthentication = () => {
         signOut(auth);
     }
 
+    const login = async (data) => {
+        checkIfIsCancelled();
+        setLoading(true);
+        setError(null);
+
+        try {
+            await signInWithEmailAndPassword(auth, data.email, data.password);
+
+            setLoading(false);
+        } catch (error) {
+            let systemErrorMessage
+            if (error.message.includes("user-not-found")) {
+                systemErrorMessage = "Usuário não encontrado.";
+            } else if (error.message.includes("wrong-password")) {
+                systemErrorMessage = "Senha incorreta.";
+            } else {
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde.";
+            }
+            setError(systemErrorMessage);
+            setLoading(false);
+        }   
+    }
+
     useEffect(() => {
         return () => setCancelled(true);
     }, []);
 
     return {
-        auth, createUser, error, loading, logout
+        auth, createUser, error, loading, logout, login
     }
 }
