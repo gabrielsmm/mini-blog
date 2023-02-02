ad2f96257e892c69e7ec791bcf6bad3e5a6dc03d
gabrielsmm <gabrielsmm251@gmail.com>
2023-02-01 21:54:43 -0300

    Exibindo post individualmente

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Register from './pages/Register/Register';
 import CreatePost from './pages/CreatePost/CreatePost';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Search from './pages/Search/Search';
+import Post from './pages/Post/Post';
 
 import { AuthProvider } from './context/AuthContext';
 import { useEffect, useState } from 'react';
@@ -43,6 +44,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/search" element={<Search />} />
+              <Route path="/posts/:id" element={<Post />} />
               <Route path="/login" element={!user ? <Login /> : <Navigate to="/" /> } />
               <Route path="/register" element={!user ? <Register /> : <Navigate to="/" /> } />
               <Route path="/posts/create" element={user ? <CreatePost /> : <Navigate to="/login" /> } />
