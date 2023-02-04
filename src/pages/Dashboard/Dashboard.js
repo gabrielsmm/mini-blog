afe854ef8ab0c7db250c64082f2a38536a780693
gabrielsmm <gabrielsmm251@gmail.com>
2023-02-04 14:12:51 -0300

    Concluindo estrutura da dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -11,8 +11,16 @@ const Dashboard = () => {
 
   const { documents: posts, error, loading } = useFetchDocuments("posts", null, uid);
 
+  const deleteDocument = (id) => {
+
+  }
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
   return (
-    <div>
+    <div className={styles.dashboard}>
       <h2>Dashboard</h2>
       <p>Gerencie os seus posts</p>
       {posts && posts.length === 0 ? (
@@ -21,14 +29,23 @@ const Dashboard = () => {
           <Link to="/posts/create" className="btn">Criar primeiro post</Link>
         </div>
       ) : (
-        <div>
-          <p>Tem posts!</p>
-        </div>
+        <>
+          <div className={styles.post_header}>
+            <span>Título</span>
+            <span>Ações</span>
+          </div>
+          {posts && posts.map((post) => (
+            <div key={post.id} className={styles.post_row}>
+              <p>{post.title}</p>
+              <div>
+                <Link to={`/posts/${post.id}`} className="btn btn-outline">Ver</Link>
+                <Link to={`/posts/edit/${post.id}`} className="btn btn-outline">Editar</Link>
+                <button onClick={() => deleteDocument(post.id)} className="btn btn-outline btn-danger">Excluir</button>
+              </div>
+            </div>
+          ))}
+        </>
       )}
-
-      {posts && posts.map((post) => (
-        <h3>{post.title}</h3>
-      ))}
     </div>
   );
 };
